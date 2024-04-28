56618c033212217bbc86c4aa7d8388d5656609f2
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-04-28 12:43:38 +0100

    Made the two functions getComputer/HumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+	let choice = Math.floor(Math.random() * 3);
+	switch (choice) {
+		case 0:
+			return "rock";
+		case 1:
+			return "paper";
+		case 2:
+			return "scissors";
+	}
+}
+
+function getHumanChoice() {
+	let choice = prompt("Type rock, paper or scissors", "");
+	if (choice === "rock" ||
+		choice === "paper" ||
+		choice === "scissors" ||
+		choice === "cancel") {
+		return choice;
+	} else {
+		alert( "Invalid input, try again, or type 'cancel' to exit." );
+		return getHumanChoice();
+	}
+}
+
